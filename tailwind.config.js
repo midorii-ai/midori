66da8e904ab7486091e008e0226fac94e53ddcd9
Suryansh Chauhan <suryanshchauhan2005@gmail.com>
2023-08-03 18:37:52 +0530

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    future: {
-        hoverOnlyWhenSupported: true,
-    },
-    theme: {
-        extend: {
-            backgroundImage: {
-                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-                "gradient-conic":
-                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-            },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  theme: {
+    extend: {
+      colors: {
+        pallete: {
+          1: "#22577a",
+          2: "#FFFCE6",
+          3: "#57CC99",
+          4: "#80ed99",
+          5: "#c7f9cc",
         },
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
-
